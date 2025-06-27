fbc91ee319251c24e5fe8d8f6e6fc539c51c89b9
jasmin <jasmin.nidhiimpex@gmail.com>
2025-06-27 18:00:37 +0530

    fix

--- src/component/AdminFormData.js
+++ src/component/AdminFormData.js
@@ -313,7 +313,7 @@ Object.keys(row).forEach((key) => {
             </>
           )}
           <Link to="/formData">
-            <button className="bg-yellow-600 hover:bg-yellow-700 text-white px-4 py-2 rounded shadow">+ Add Data</button>
+            <button className="bg-yellow-600 hover:bg-yellow-700 text-white px-4 py-2 rounded shadow">Add Data</button>
           </Link>
           <Link to="/signup">
             <button className="bg-yellow-400 hover:bg-yellow-500 text-black px-4 py-2 rounded shadow">Sign Up</button>
